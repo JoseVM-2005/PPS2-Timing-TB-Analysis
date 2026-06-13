caef4c07d09a749f5526a049b9e4fb398c84b008
jose.valentim.miguel <jose.valentim.miguel@tecnico.ulisboa.pt>
2026-06-13 22:04:56 +0100

    not sure is commit was successful

--- PPS_TB_202505_RunDB.py
+++ PPS_TB_202505_RunDB.py
@@ -614,7 +614,7 @@ run_config_info = {
 
 run_info = {
     "Run013_J": RunInformation(
-        name = "Run 013",
+        name = "Run 013_J",
         trigger_mode = "self",
         trigger_channels = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 23),
         data_format = "binary",
